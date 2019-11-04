ae7cee0280f54020083a384c842adefd99dfbcc0
glottotopia <sebastian.nordhoff@glottotopia.de>
2019-11-04 14:23:56 +0100

    get rid of quotation marks, add trailing commas

--- getproofreaders.sh
+++ getproofreaders.sh
@@ -4,4 +4,4 @@ echo LangSci ID is $LANGSCIID
 PHIDtmp=`curl -s "https://paperhive.org/api/document-items/by-document/external?type=langsci&id="$LANGSCIID |jq '.["documentItems"][0]["id"]'`
 PHID=${PHIDtmp:1:-1}
 echo Paperhive ID is $PHID
-curl -s "https://paperhive.org/api/discussions?documentItem=$PHID"| jq  ".discussions[] | .author.displayName"|sort -u 
+curl -s "https://paperhive.org/api/discussions?documentItem=$PHID"| jq  ".discussions[] | .author.displayName"|sort -u |sed 's/"//'|sed 's/"/,/'
