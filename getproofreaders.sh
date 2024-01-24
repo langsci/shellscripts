1ee55764a41d2b1b9469322a1187b2102f99582e
glottotopia <sebastian.nordhoff@glottotopia.de>
2024-01-24 10:43:16 +0100

    allow for paperhive IDs as argument

--- getproofreaders.sh
+++ getproofreaders.sh
@@ -1,7 +1,13 @@
 # LANGSCIID=${PWD##*/}
-LANGSCIID=$1
-echo LangSci ID is $LANGSCIID
-PHIDtmp=`curl -s "https://paperhive.org/api/document-items/by-document/external?type=langsci&id="$LANGSCIID |jq '.["documentItems"][0]["id"]'`
-PHID=${PHIDtmp:1:-1}
+ARG=$1
+if [ ${#ARG}  -gt 4 ]
+then
+    PHID=$ARG
+else
+    LANGSCIID=$1
+    echo LangSci ID is $LANGSCIID
+    PHIDtmp=`curl -s "https://paperhive.org/api/document-items/by-document/external?type=langsci&id="$LANGSCIID |jq '.["documentItems"][0]["id"]'`
+    PHID=${PHIDtmp:1:-1}
+fi
 echo Paperhive ID is $PHID
 curl -s "https://paperhive.org/api/discussions?documentItem=$PHID"| jq  ".discussions[] | .author.displayName"|sort -u |sed 's/"//'|sed 's/"/,/'
